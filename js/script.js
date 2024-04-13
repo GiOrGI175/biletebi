0a2c5f157cc84576c17e95d290d0d5a5d45dcae2
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-04-13 19:29:53 +0400

    last commit
    all done

--- js/script.js
+++ js/script.js
@@ -14,7 +14,18 @@ switch (true) {
       let evening_price = 5;
       price = evening_price + price;
     }
-    alert(`ბილეთის ფასი თქვენთვის არის : ${price} ლარი.`);
+    let fasdaakleba;
+    let num = Math.floor(Math.random() * 3) + 1;
+    if (num % 2 === 0) {
+      price3 = price3 - 5;
+      let shidafasdaakleba =
+        'გილოცავთ თქვენ გაქვთ ფასდაკლების ბილეთი და თანხას დააკლდა 5 ლარი.';
+      fasdaakleba = shidafasdaakleba;
+    } else {
+      shidafasdaakleba = 'თქვენ არ გაქვთ ფასდაკლეის ბილეთი';
+      fasdaakleba = shidafasdaakleba;
+    }
+    alert(`ბილეთის ფასი თქვენთვის არის : ${price} ლარი. ${fasdaakleba}`);
     break;
   case age > 10 && age < 65:
     let price2 = 15;
@@ -25,7 +36,18 @@ switch (true) {
       let evening_price2 = 5;
       price2 = evening_price2 + price2;
     }
-    alert(`ბილეთის ფასი თქვენთვის არის : ${price2} ლარი.`);
+    let fasdaakleba2;
+    let num2 = Math.floor(Math.random() * 3) + 1;
+    if (num2 % 2 === 0) {
+      price2 = price2 - 5;
+      let shidafasdaakleba =
+        'გილოცავთ თქვენ გაქვთ ფასდაკლების ბილეთი და თანხას დააკლდა 5 ლარი.';
+      fasdaakleba2 = shidafasdaakleba;
+    } else {
+      shidafasdaakleba = 'თქვენ არ გაქვთ ფასდაკლეის ბილეთი';
+      fasdaakleba2 = shidafasdaakleba;
+    }
+    alert(`ბილეთის ფასი თქვენთვის არის : ${price2} ლარი. ${fasdaakleba2} `);
     break;
   case age > 65:
     let price3 = 10;
@@ -36,6 +58,17 @@ switch (true) {
       let evening_price3 = 5;
       price3 = evening_price3 + price3;
     }
-    alert(`ბილეთის ფასი თქვენთვის არის : ${price3} ლარი.`);
+    let fasdaakleba3;
+    let num3 = Math.floor(Math.random() * 3) + 1;
+    if (num3 % 2 === 0) {
+      price3 = price3 - 5;
+      let shidafasdaakleba =
+        'გილოცავთ თქვენ გაქვთ ფასდაკლების ბილეთი და თანხას დააკლდა 5 ლარი.';
+      fasdaakleba3 = shidafasdaakleba;
+    } else {
+      shidafasdaakleba = 'თქვენ არ გაქვთ ფასდაკლეის ბილეთი';
+      fasdaakleba3 = shidafasdaakleba;
+    }
+    alert(`ბილეთის ფასი თქვენთვის არის : ${price3} ლარი. ${fasdaakleba3}`);
     break;
 }
