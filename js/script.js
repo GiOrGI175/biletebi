5271c39b897edab4ec6e5aa62f12530df21395d0
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-04-13 23:02:14 +0400

    last commit
    all fix

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-let age = prompt('Enter your age:');
+let age = Number(prompt('Enter your age:'));
 
 let movie_type = prompt('Enter movie type: regular or 3D');
 
 let time = prompt('Enter time of day: morning or evening');
 
+let shidafasdaakleba;
+
 switch (true) {
   case age < 10:
     let price = 5;
-    if ((movie_type = '3D')) {
+    if (movie_type === '3D') {
       price = price + 5;
     }
-    if ((time = 'evening')) {
+    if (time === 'evening') {
       let evening_price = 5;
       price = evening_price + price;
     }
     let fasdaakleba;
     let num = Math.floor(Math.random() * 3) + 1;
     if (num % 2 === 0) {
-      price3 = price3 - 5;
-      let shidafasdaakleba =
+      price = price - 5;
+      shidafasdaakleba =
         'გილოცავთ თქვენ გაქვთ ფასდაკლების ბილეთი და თანხას დააკლდა 5 ლარი.';
       fasdaakleba = shidafasdaakleba;
     } else {
@@ -29,10 +31,10 @@ switch (true) {
     break;
   case age > 10 && age < 65:
     let price2 = 15;
-    if ((movie_type = '3D')) {
+    if (movie_type === '3D') {
       price2 = price2 + 5;
     }
-    if ((time = 'evening')) {
+    if (time === 'evening') {
       let evening_price2 = 5;
       price2 = evening_price2 + price2;
     }
@@ -51,10 +53,10 @@ switch (true) {
     break;
   case age > 65:
     let price3 = 10;
-    if ((movie_type = '3D')) {
+    if (movie_type === '3D') {
       price3 = price3 + 5;
     }
-    if ((time = 'evening')) {
+    if (time === 'evening') {
       let evening_price3 = 5;
       price3 = evening_price3 + price3;
     }
